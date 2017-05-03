6482fa841f01ff44d103b10a6c59777ef51b3ecf
Beth Hansen and Michael Koceja <michael@>
2017-05-03 09:02:45 -0700

    initial file setup, ran the simpliest function which is return false, it works

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
   //
   $("form#leap-year").submit(function(event) {
-    //call a leap-year function pass it the year as an argument
+    //call a leap-year function pass the year as an argument
     event.preventDefault();
     //this will stop the function at the end of the run
     var year = parseInt($("input#year").val());
-    //
+    //this tells the computer that the user input is a number "parseInt"
     var result = leapYear(year);
-    //call the leap year function and pass it the year
+    //call the leap year function and pass the year as a parameter
     $("#result").text(result);
     //the result of a true or false
   });
